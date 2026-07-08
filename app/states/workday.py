efa0f59585993041d16e9672d68327563277c6fe
kubarevde <Kubarev.work@gmail.com>
2026-07-08 08:55:22 +0700

    Update workday.py

--- app/states/workday.py
+++ app/states/workday.py
@@ -3,6 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class StartWork(StatesGroup):
     location = State()
+    geo = State()
     work_type = State()
     equipment = State()
     comment = State()
